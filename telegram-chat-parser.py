2d89fc325fddea7856a46aa8146b0e02b8bb1068
Artur Rodrigues <artur.rodrigues26@gmail.com>
2020-12-23 11:49:36 -0300

    First version

---
+++ telegram-chat-parser.py
@@ -0,0 +1,67 @@
+"""
+file:         telegram-chat-parser.py
+author:       Artur Rodrigues Rocha Neto
+email:        artur.rodrigues26@gmail.com
+github:       keizerzilla
+description:  Script to parse a Telegram chat history JSON file into a tabular format (CSV).
+requirements: Python 3.x
+"""
+
+import sys
+import csv
+import json
+
+if len(sys.argv) != 3:
+    print("ERROR: incorrect number of arguments!")
+    print("How to:")
+    print("    python3 telegram-chat-parser.py <chat_history_json> <output_csv>")
+    print("Example:")
+    print("    python3 telegram-chat-parser.py movies_group.json chat_movies.csv")
+    sys.exit()
+
+result_filepath = sys.argv[1]
+output_filepath = sys.argv[2]
+template = ["id", "from", "from_id", "date", "text", "reply_to_message_id"]
+
+with open(result_filepath, "r", encoding="utf-8") as infile:
+    with open(output_filepath, "w", encoding="utf-8") as outfile:
+        writer = csv.DictWriter(outfile, template, dialect="unix", quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        
+        contents = infile.read()
+        jdata = json.loads(contents)
+        
+        for message in jdata["messages"]:
+            if message["type"] != "message" or len(message["text"]) == 0:
+                continue
+            
+            id_ = message["id"]
+            from_ = message["from"]
+            from_id_ = message["from_id"]
+            date_ = message["date"]
+            text_ = message["text"]
+            reply_to_message_id_ = message["reply_to_message_id"] if "reply_to_message_id" in message else ""
+            
+            if type(text_) == list:
+                new_text = ""
+                for part in text_:
+                    if type(part) == str:
+                        new_text += part
+                    elif type(part) == dict:
+                        new_text += part["text"]
+                text_ = new_text
+            
+            text_ = text_.replace("\n", " ")
+            
+            row = {
+                "id"                  : id_,
+                "from"                : from_,
+                "from_id"             : from_id_,
+                "date"                : date_,
+                "text"                : text_,
+                "reply_to_message_id" : reply_to_message_id_,
+            }
+            
+            writer.writerow(row)
+
+print("Parse of {} into {} finished!".format(result_filepath, output_filepath))
